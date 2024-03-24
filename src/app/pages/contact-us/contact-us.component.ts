49dbe207c3e3897693f072971f2ede3e147d4b5a
Kevin Laig <laig.kevin@gmail.com>
2024-03-24 11:50:18 +0200

    Finished

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
+
+  constructor(private _snackBar: MatSnackBar) {}
+
+  openSnackBar() {
+    duration: 3000;
+    this._snackBar.open("Email sent! We appreciate your feedback.", "close" ,{ duration: 3000 });
+    duration: 3000;
+  }
+
 
 }
